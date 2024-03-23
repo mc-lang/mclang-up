ff1166e64871f0ab6f46a9cdd4260697ef019ff7
MCorange99 <mcorange@mcorangehq.xyz>
2024-03-23 04:24:51 +0200

    Fixed warnings

--- src/install.rs
+++ src/install.rs
@@ -1,4 +1,4 @@
-use std::{path::{PathBuf, Path}, process::{Command, Stdio}};
+use std::{path::PathBuf, process::{Command, Stdio}};
 use color_eyre::Result;
 use eyre::eyre;
 
@@ -202,19 +202,4 @@ fn get_install_branch() -> Result<String>{
         }
     }
     Ok(p)
-}
-
-
-fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> Result<()> {
-    std::fs::create_dir_all(&dst)?;
-    for entry in std::fs::read_dir(src)? {
-        let entry = entry?;
-        let ty = entry.file_type()?;
-        if ty.is_dir() {
-            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
-        } else {
-            std::fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
-        }
-    }
-    Ok(())
 }
